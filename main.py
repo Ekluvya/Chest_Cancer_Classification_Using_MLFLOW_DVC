6b971e446a7031542c66901bed9634ef50f178cd
Ekluvya <47681673+Ekluvya@users.noreply.github.com>
2024-05-01 15:56:21 +0530

    Added model trainer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -20,6 +21,18 @@ STAGE_NAME = "Prepare Base Model Stage"
 try:
     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Training Stage"
+
+    
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:

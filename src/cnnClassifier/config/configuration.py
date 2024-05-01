79f6a5efd864244ea4f9bee62ba01f9968836df7
Ekluvya <47681673+Ekluvya@users.noreply.github.com>
2024-05-01 16:58:16 +0530

    Added Model Evaluation and tracking using MLFLOW

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 import os
 from cnnClassifier.constants import *
-from cnnClassifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig, EvaluationConfig, PrepareBaseModelConfig, TrainingConfig)
 from cnnClassifier.utils.common import read_yaml, create_directories
 
 class ConfigurationManager:
@@ -69,4 +69,15 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/Ekluvya/Chest_Cancer_Classification_Using_MLFLOW_DVC.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
       
